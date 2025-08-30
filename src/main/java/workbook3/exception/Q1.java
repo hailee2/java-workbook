a33de1f1e25e94f8bb5e4780da507eb9550a8a96
hailee2 <slowslow9150@gmail.com>
2025-08-30 15:09:17 +0900

    feat: [exception] Q1 구현 및 주석 추가

--- src/main/java/workbook3/exception/Q1.java
+++ src/main/java/workbook3/exception/Q1.java
@@ -9,7 +9,8 @@ public class Q1 {
         int b = sc.nextInt();
         try {
             // TODO: a/b 결과 출력
-        } catch (ArithmeticException e) {
+            System.out.println(a/b);
+        } catch (ArithmeticException e) {               //정수 연산에서 잘못된 산술 연산이 발생했을 때 자동으로 JVM이 던지는 예외
             System.out.println("Division by zero");
         }
     }
