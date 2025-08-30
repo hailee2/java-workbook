100f591a2d22c0d56f486c4c4b7a3e5c5593ee05
hailee2 <slowslow9150@gmail.com>
2025-08-30 16:19:09 +0900

    feat: [exception] Q2 구현 및 주석 추가

--- src/main/java/workbook3/exception/Q2.java
+++ src/main/java/workbook3/exception/Q2.java
@@ -12,9 +12,12 @@ public class Q2 {
         }
         try {
             // TODO: index 입력 받기
+            int index = sc.nextInt();                           //배열의 크기는 n. index는 n보다 작아야함.
             // TODO: arr[index] 출력
+            System.out.println(arr[index]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Index out of bounds");
         }
+    sc.close();
     }
 }
