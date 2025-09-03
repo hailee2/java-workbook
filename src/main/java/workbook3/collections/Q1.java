2395a1c97f12794f770aed5fbd8908f2835e50c1
hailee2 <slowslow9150@gmail.com>
2025-09-03 19:55:32 +0900

    feat: [collections] Q1 구현 및 주석 추가

--- src/main/java/workbook3/collections/Q1.java
+++ src/main/java/workbook3/collections/Q1.java
@@ -9,5 +9,13 @@ public class Q1 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // TODO: 입력받은 문자열을 N개를 ArrayList에 저장한 후 모든 원소를 순서대로 출력하는 프로그램을 작성하시오.
+        List<String> str = new ArrayList<>();
+        for(int i = 0; i<n; i++){
+            str.add(sc.next());             //리스트는 배열처럼 인덱스를 이용해 대입할 수 없음. add()메서드를 사용해 요소 추가 가능
+        }
+        for (String s : str) {
+            System.out.println(s);
+        }
+        sc.close();
     }
 }
