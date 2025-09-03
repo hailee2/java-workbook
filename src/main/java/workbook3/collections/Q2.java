42d2b97bdf0edee287bf044f1d09d28494dd6be7
hailee2 <slowslow9150@gmail.com>
2025-09-03 20:58:50 +0900

    feat: [collections] Q2 구현

--- src/main/java/workbook3/collections/Q2.java
+++ src/main/java/workbook3/collections/Q2.java
@@ -8,8 +8,12 @@ public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        List<Integer> list = new ArrayList<>();
-        for(int i=1;i<=n;i++) list.add(i);
+        List<Integer> list = new ArrayList<>();     //빈 리스트 생성
+        for(int i=1;i<=n;i++) list.add(i);          //리스트에 1부터 n까지 채워 넣음
         // TODO: list를 역순으로 출력
+        for(int i = list.size()-1; i>=0; i--){      //list의 인덱스를 거꾸로 출력
+            System.out.println(list.get(i));        //리스트는 메서드로 접근해야 함 |예 : add(), get(), set()
+        }
+        sc.close();
     }
 }
