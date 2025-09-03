aff9c2e4f9b5573898292b280bc07bbc215bce63
hailee2 <slowslow9150@gmail.com>
2025-09-03 22:11:00 +0900

    feat: [collections] Q6

--- src/main/java/workbook3/collections/Q6.java
+++ src/main/java/workbook3/collections/Q6.java
@@ -18,6 +18,17 @@ public class Q6 {
             list2.add(sc.nextInt());
         }
 
-        // TODO: 합집합과 교집합 구해 출력
+        // TODO: 합집합과 교집합 구해 출력 (HashSet 이용)
+        //합집합 : 중복 없는 집합. 두 리스트를 addAll() 메서드로 합치기
+        Set<Integer> union = new HashSet<>(list1);      //ArrayList를 HashSet으로 변환
+        union.addAll(list2);
+        //교집합 : 두 집합에 있는 원소만 남기. retainAll()메서드 사용
+        Set<Integer> intersection = new HashSet<>(list1);
+        intersection.retainAll(list2);
+
+        System.out.println("합집합 : "+ union);
+        System.out.println("교집합 : "+ intersection);
+
+        sc.close();
     }
 }
