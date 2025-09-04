9e8d9606e0c71e7eff00d55d6d2d33389f08e9ae
hailee2 <slowslow9150@gmail.com>
2025-09-04 21:55:08 +0900

    feat: [genericenum] Q1 주석 추가

--- src/main/java/workbook3/genericenum/Q1.java
+++ src/main/java/workbook3/genericenum/Q1.java
@@ -2,16 +2,17 @@ package main.java.workbook3.genericenum;
 
 import java.util.Scanner;
 
-class GenericBox<T> {
-    T value;
+class GenericBox<T> {                             //제네릭 클래스
+    T value;                                      //필드. T 타입
 
     GenericBox(T value) {
         this.value = value;
-    }
+    }   //생성자의 매개변수는 value (타입이 T)
+                                                  //T는 클래스에서 선언한 "타입 매개변수"
 
     T get() {
         return value;
-    }
+    }                     //T value의 값을 반환하는 get() 메서드
 }
 
 public class Q1 {
@@ -21,7 +22,8 @@ public class Q1 {
         String s = sc.next();
         GenericBox<Integer> box1 = new GenericBox<>(i);
         GenericBox<String> box2 = new GenericBox<>(s);
-        System.out.println(box1.get());
-        System.out.println(box2.get());
+        System.out.println(box1.get());     // i 출력
+        System.out.println(box2.get());     // s 출력
+        sc.close();
     }
 }
